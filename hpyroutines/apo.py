78314510572cbf28f37074c1af384b72b53d5918
Federico Bianchini <federico.bianxini@gmail.com>
2017-10-30 12:10:21 +1100

    added throw circles in the skt

--- hpyroutines/apo.py
+++ hpyroutines/apo.py
@@ -3,6 +3,179 @@ import healpy as hp
 
 arcmin2rad = np.pi / 180. / 60. 
 
+def correctTheta(theta):
+    if theta < 0.:
+        return -theta
+    if theta > np.pi:
+        return 2 * np.pi - theta
+    return theta
+
+
+def cosApodization(sigma, x):
+   if x > sigma:
+       return 1
+   return 1 - np.cos(np.pi * x / (2 * sigma))
+
+def gaussApodization(sigma, x):
+   if x > sigma:
+       return 1
+    # check(sigma > 0, "invalid sigma = " << sigma);
+    return 1 - np.exp(- 9 * x * x / (2 * sigma * sigma))
+
+# def apodize(angle, Healpix_Map<double>& result, type) const
+#     check(type >= COSINE_APODIZATION && type < APODIZATION_TYPE_MAX, "invalid apodization type");
+#     check(angle > 0, "invalid angle = " << angle);
+
+
+#     result.SetNside(mask_.Nside(), mask_.Scheme());
+#     result.Import(mask_);
+
+#     nPix = result.Npix();
+# 	pixSize = np.sqrt(4 * np.pi / nPix);
+
+#     Healpix_Base2 base2(result.Nside(), result.Scheme(), SET_NSIDE);
+    
+#     print("Finding the edge pixels...")
+
+#     std::vector<Math::ThreeVectorDouble> edge;
+
+#     for i in xrange(nPix):
+#         if result[i] == 0:
+#             continue
+#         isOnEdge = False
+#         fix_arr<int64, 8> neighbors;
+#         base2.neighbors(i, neighbors);
+#         const long s = neighbors.size();
+#         for j = 0; j < s; ++j)
+#         {
+#             if(result[neighbors[j]] == 0)
+#                 isOnEdge = true;
+#         }
+#         double theta, phi;
+#         result.Scheme() == NEST ? pix2ang_nest(result.Nside(), i, &theta, &phi) : pix2ang_ring(result.Nside(), i, &theta, &phi);
+#         long index;
+#         /*
+#         result.Scheme() == NEST ? ang2pix_nest(result.Nside(), correctTheta(theta + pixSize), phi, &index) : ang2pix_ring(result.Nside(), correctTheta(theta + pixSize), phi, &index);
+#         if(result[index] == 0)
+#             isOnEdge = true;
+#         result.Scheme() == NEST ? ang2pix_nest(result.Nside(), correctTheta(theta - pixSize), phi, &index) : ang2pix_ring(result.Nside(), correctTheta(theta - pixSize), phi, &index);
+#         if(result[index] == 0)
+#             isOnEdge = true;
+#         result.Scheme() == NEST ? ang2pix_nest(result.Nside(), theta, phi + pixSize, &index) : ang2pix_ring(result.Nside(), theta, phi + pixSize, &index);
+#         if(result[index] == 0)
+#             isOnEdge = true;
+#         result.Scheme() == NEST ? ang2pix_nest(result.Nside(), theta, phi - pixSize, &index) : ang2pix_ring(result.Nside(), theta, phi - pixSize, &index);
+#         if(result[index] == 0)
+#             isOnEdge = true;
+#         */
+#         if(isOnEdge)
+#         {
+#             edge.push_back(Math::ThreeVectorDouble(std::sin(theta) * std::cos(phi), std::sin(theta) * std::sin(phi), std::cos(theta)));
+#             //ang2pix_nest(edgeNSide, theta, phi, &index);
+#             //edgeMap[index] = 1;
+#             //edgeMap[i] = 1;
+#         }
+#         meter.advance();
+#     }
+#     output_screen("OK" << std::endl);
+
+#     output_screen("Found " << edge.size() << " edge pixels." << std::endl);
+
+#     /*
+#     fitshandle outh;
+#     outh.create("edge.fits");
+#     write_Healpix_map_to_fits(outh, edgeMap, PLANCK_FLOAT64);
+#     */
+
+#     output_screen("Apodizing mask..." << std::endl);
+#     ProgressMeter meter1(nPix);
+# #ifdef COSMO_OMP
+#     omp_lock_t lock;
+#     omp_init_lock(&lock);
+# #endif
+# #pragma omp parallel for default(shared)
+#     for(long i = 0; i < nPix; ++i)
+#     {
+#         if(result[i] == 0)
+#         {
+# #ifdef COSMO_OMP
+#             omp_set_lock(&lock);
+# #endif
+#             meter1.advance();
+# #ifdef COSMO_OMP
+#             omp_unset_lock(&lock);
+# #endif
+#             continue;
+#         }
+
+#         double theta, phi;
+#         result.Scheme() == NEST ? pix2ang_nest(result.Nside(), i, &theta, &phi) : pix2ang_ring(result.Nside(), i, &theta, &phi);
+#         Math::ThreeVectorDouble current(std::sin(theta) * std::cos(phi), std::sin(theta) * std::sin(phi), std::cos(theta));
+#         double minDistance = 2 * Math::pi;
+#         for(int j = 0; j < edge.size(); ++j)
+#         {
+#             const double distance = std::acos(current * edge[j]);
+#             if(distance < minDistance)
+#                 minDistance = distance;
+#         }
+#         result[i] = (type == COSINE_APODIZATION ? cosApodization(angle, minDistance) : gaussApodization(angle, minDistance));
+
+# #ifdef COSMO_OMP
+#         omp_set_lock(&lock);
+# #endif
+#         meter1.advance();
+# #ifdef COSMO_OMP
+#         omp_unset_lock(&lock);
+# #endif
+#     }
+
+def angular_distance_from_mask(mask):
+    """
+    For each pixel of a Healpix map, return the smallest angular distance
+    to a set of masked pixels (of value True), in degrees.
+    Parameter
+    ---------
+    maskok : boolean Healpix map
+        The Healpix mask that defines the set of masked pixels (of value True)
+        whose smallest angular distance to each pixel of a Healpix map of same
+        nside is computed.
+    """
+    nside = hp.npix2nside(len(mask))
+
+    # get the list of pixels on the external border of the mask
+    ip = np.arange(12*nside**2)[~mask]
+    neigh = hp.get_all_neighbours(nside, ip)
+    nn = np.unique(neigh.ravel())
+    if nn[0] == -1:
+        nn = nn[1:]
+    nn = nn[mask[nn]]
+
+    # get unit vectors for border and inner pixels
+    vecpix_inner = np.array(hp.pix2vec(nside, ip))
+    vecpix_outer = np.array(hp.pix2vec(nside, nn))
+
+    # get angles between the border pixels and inner pixels
+    cosang = np.dot(vecpix_inner.T, vecpix_outer)
+    mapang = np.zeros(12*nside**2)
+    mapang[~mask] = np.degrees(np.arccos(np.max(cosang, axis=1)))
+    return mapang
+
+
+def apodize_mask(maskok, fwhm_deg):
+    """
+    Apodize a mask.
+    Parameters
+    ----------
+    maskok : boolean Healpix map
+        The mask to be apodized (of value True).
+    fwhm_deg : float
+        The FWHM of the apodization function, counted from the mask edges,
+        in degrees.
+    """
+    sigma_deg = fwhm_deg / np.sqrt(8 * np.log(2))
+    mapang = angular_distance_from_mask(~maskok)
+    return 1 - np.exp(-0.5 * mapang**2 / sigma_deg**2)
+
 def SmoothMask(mask, fwhm, pcut=1e-2, order='ring', verbose=False):
 	"""
 	mask is binary healpix mask
--- hpyroutines/utils.py
+++ hpyroutines/utils.py
@@ -93,38 +93,38 @@ def GetCountsMap(sky1, sky2, nside, coord='G', nest=False, rad=False, sqdeg=Fals
 	return counts_map
 
 def Counts2Delta(counts, mask=None):
-    """
-    Converts a number counts Healpix map into a density contrast map.
+	"""
+	Converts a number counts Healpix map into a density contrast map.
 
-    Parameters
-    ----------
-    counts : array-like
-    	Input healpix map with number counts
+	Parameters
+	----------
+	counts : array-like
+		Input healpix map with number counts
 
-    mask : array-like
-    	Healpix map containing the mask 
+	mask : array-like
+		Healpix map containing the mask 
 
-    Note
-    ----
-    * use only binary mask.
-    """
-    counts = np.asarray(counts)
+	Note
+	----
+	* use only binary mask.
+	"""
+	counts = np.asarray(counts)
 
-    if not hp.isnpixok(counts.size):
-        raise ValueError("Not a valid healpix map!")
+	if not hp.isnpixok(counts.size):
+		raise ValueError("Not a valid healpix map!")
 
-    if mask is not None:
-        mask   = np.asarray(mask)
-        counts = hp.ma(counts)
-        counts.mask = np.logical_not(mask)
-    
-    mean  = np.mean(counts)
-    delta = (counts - mean) / mean
+	if mask is not None:
+		mask   = np.asarray(mask)
+		counts = hp.ma(counts)
+		counts.mask = np.logical_not(mask)
+	
+	mean  = np.mean(counts)
+	delta = (counts - mean) / mean
 
-    if mask is not None:
-	    delta = delta.filled(counts.fill_value) # To avoid pixels with fill_value = 1e+20
-    
-    return delta
+	if mask is not None:
+		delta = delta.filled(counts.fill_value) # To avoid pixels with fill_value = 1e+20
+	
+	return delta
 
 def GuessMask(sky1, sky2, nside, coord='G', nest=False, rad=False):
 	"""
@@ -162,15 +162,15 @@ def GuessMask(sky1, sky2, nside, coord='G', nest=False, rad=False):
 	return mask
 
 def GetGalMask(nside, lat=None, fsky=None, nest=False):
-    """
-    Returns a symmetric Galactic Mask in Healpix format at nside resolution.
-    Pixels with latitude < |lat| deg are set to 0.
-    Otherwise you input the fsky and evaluates the required latitude.
+	"""
+	Returns a symmetric Galactic Mask in Healpix format at nside resolution.
+	Pixels with latitude < |lat| deg are set to 0.
+	Otherwise you input the fsky and evaluates the required latitude.
 
-    Parameters
-    ----------
-    nside : int
-    	Resolution of the Healpix pixelization scheme
+	Parameters
+	----------
+	nside : int
+		Resolution of the Healpix pixelization scheme
 
 	lat : float
 		Latitude of the galatic cut
@@ -180,19 +180,19 @@ def GetGalMask(nside, lat=None, fsky=None, nest=False):
 
 	nest : bool [def=False]
 		If True, nested pixelation scheme is considered for the output pixels
-    """
-    if lat is None:
-        if fsky is not None:
-            lat = np.rad2deg(np.arcsin(1. - fsky))
-        else:
-            raise ValueError("Missing lat or fsky !")
+	"""
+	if lat is None:
+		if fsky is not None:
+			lat = np.rad2deg(np.arcsin(1. - fsky))
+		else:
+			raise ValueError("Missing lat or fsky !")
 
-    mask = np.zeros(hp.nside2npix(nside))
-    theta_cut = np.deg2rad(90. - lat)
-    mask[np.where((hp.pix2ang(nside, np.arange(mask.size), nest=nest))[0] >= (np.pi - theta_cut))] = 1.
-    mask[np.where((hp.pix2ang(nside, np.arange(mask.size), nest=nest))[0] <= theta_cut)] = 1.
+	mask = np.zeros(hp.nside2npix(nside))
+	theta_cut = np.deg2rad(90. - lat)
+	mask[np.where((hp.pix2ang(nside, np.arange(mask.size), nest=nest))[0] >= (np.pi - theta_cut))] = 1.
+	mask[np.where((hp.pix2ang(nside, np.arange(mask.size), nest=nest))[0] <= theta_cut)] = 1.
 
-    return mask
+	return mask
 
 def GetSourcesMask(sky1, sky2, nside, radius, coord='G', nest=False, rad=False):
 	'''
@@ -211,7 +211,7 @@ def GetSourcesMask(sky1, sky2, nside, radius, coord='G', nest=False, rad=False):
 		Resolution of the Healpix pixelization scheme
 
 	radius : array-like
-	    The radius of the disc in degrees, can be different for each source
+		The radius of the disc in degrees, can be different for each source
 
 	coord : str 'C','E' or 'G' [def]
 		Coordinate system of the output. If coord='C', *no* rotation is applied
@@ -225,7 +225,7 @@ def GetSourcesMask(sky1, sky2, nside, radius, coord='G', nest=False, rad=False):
 	Returns
 	-------
 	psmask : array-like
-	    A Healpix map with 0 values inside the discs
+		A Healpix map with 0 values inside the discs
 	'''
 
 	pspix  = Sky2Hpx(sky1, sky2, nside, coord=coord, nest=nest, rad=rad)
@@ -241,144 +241,246 @@ def GetSourcesMask(sky1, sky2, nside, radius, coord='G', nest=False, rad=False):
 
 	return psmask
 
+def ThrowCircleMasksOnTheSky(radius, mask=None, nside=2048, overlap=0.1, overlap_mask=0.05, fsky=0.1, max_iter=200, return_pix=False):
+
+    full_mask = np.ones(hp.nside2npix(nside))
+
+    if mask is not None:
+        full_mask *= mask
+    else:
+        mask = np.ones(hp.nside2npix(nside))
+        
+    pix_ones = np.where(full_mask == 1)[0]
+    
+    i = 0
+    xs = []
+    ys = []
+    zs = []
+    while (np.mean(full_mask) > fsky):
+        x, y, z = hp.pix2vec(nside,np.random.choice(pix_ones))
+        ipix = hp.query_disc(nside, (x, y, z), np.radians(radius))
+        cond1 = len(np.where(full_mask[ipix]==0)[0]) < overlap * len(ipix)
+        cond2 = len(np.where(mask[ipix]==0)[0]) < overlap_mask * len(ipix)
+        if cond1 and cond2:
+            full_mask[ipix] = 0.
+            pix_ones = np.where(full_mask == 1)[0]
+            i = 0 
+            xs.append(x)
+            ys.append(y)
+            zs.append(z)
+        else:
+            i += 1
+            if i > max_iter: break
+            
+    print 'created %d holes' %len(xs)
+    if i !=0:
+        print 'exiting with %d iterations'%(i)
+    print 'f_sky = %.3f' %full_mask.mean()
+    if return_pix:
+        return full_mask, (xs, ys, zs)
+    else:
+        return full_mask
+	
+def filter_highpass_1d(reclen, lmin, dl):
+    # central_freq = samp_freq/float(reclen)
+    l = np.arange(reclen)#/2+1) * central_freq
+    filt = np.ones(reclen)
+    filt[l<lmin-dl/2] = 0.0
+
+    window_for_transition_to_zero = (lmin-dl/2 <= l) * (l <= lmin+dl/2)
+    ind=np.where(window_for_transition_to_zero==True)[0]
+    reclen_transition_window = len(filt[window_for_transition_to_zero])
+    filt[window_for_transition_to_zero] = (1. - np.cos( np.pi* np.arange(reclen_transition_window) /(reclen_transition_window-1))) /2.0
+
+    return filt
+
 def GetCorrAlms(clxy, clxx, clyy, nside, lmax=None):
-    """
-    Generates correlated gaussian realizations of two fields X and Y 
-    which statistics is described by the sets of three Auto- and 
-    Cross-spectra clxx, clyy, and clxy. 
+	"""
+	Generates correlated gaussian realizations of two fields X and Y 
+	which statistics is described by the sets of three Auto- and 
+	Cross-spectra clxx, clyy, and clxy. 
 
 	Based on HealPix routine create_alm.
-    
-    Parameters
-    ----------
-    clxy :  array-like 
-    	Input cross-power spectrum C_l^{XY}
+	
+	Parameters
+	----------
+	clxy :  array-like 
+		Input cross-power spectrum C_l^{XY}
 
-    clxx :  array-like 
-    	Input auto-power  spectrum C_l^{XX} of first field
+	clxx :  array-like 
+		Input auto-power  spectrum C_l^{XX} of first field
 
-    clyy :  array-like 
-    	Input auto-power  spectrum C_l^{YY} of second field
+	clyy :  array-like 
+		Input auto-power  spectrum C_l^{YY} of second field
 
-    nside : int 
-    	Healpix resolution
+	nside : int 
+		Healpix resolution
 
-    lmax : int (optional) 
-    	Maximum multipole 
-    
+	lmax : int (optional) 
+		Maximum multipole 
+	
 
-    Returns
-    -------
-    alm_xx : array-like
-    	First field Alm coefficients
-    
-    alm_yy : array-like
-     	Second field Alm coefficients 
+	Returns
+	-------
+	alm_xx : array-like
+		First field Alm coefficients
+	
+	alm_yy : array-like
+		Second field Alm coefficients 
 
-    Notes
-    -----
-    *  Realizations are returned as *alms* 
+	Notes
+	-----
+	*  Realizations are returned as *alms* 
 
-    """
+	"""
 
-    if lmax is None:
-        lmax = 2*nside 
-        lmax = int(lmax)
+	if lmax is None:
+		lmax = 2*nside 
+		lmax = int(lmax)
 
-    tot_alm = hp.sphtfunc.Alm.getsize(lmax)  
+	tot_alm = hp.sphtfunc.Alm.getsize(lmax)  
 
-    alm_xx = np.zeros(tot_alm, dtype=complex)
-    alm_yy = np.zeros(tot_alm, dtype=complex)
+	alm_xx = np.zeros(tot_alm, dtype=complex)
+	alm_yy = np.zeros(tot_alm, dtype=complex)
 
 
-    # Loop on l: Let's deal with X field coefficients
-    # and first term rms_yy1 of the Y field coefficients
-    for l in xrange(0,lmax+1):
-        rms_xx = 0.
-        rms_yy1 = 0.
-        if clxx[l] != 0.: # To avoid division by zero
-            rms_xx = np.sqrt(clxx[l])
-            rms_yy1 = clxy[l]/rms_xx
+	# Loop on l: Let's deal with X field coefficients
+	# and first term rms_yy1 of the Y field coefficients
+	for l in xrange(0,lmax+1):
+		rms_xx = 0.
+		rms_yy1 = 0.
+		if clxx[l] != 0.: # To avoid division by zero
+			rms_xx = np.sqrt(clxx[l])
+			rms_yy1 = clxy[l]/rms_xx
 
-        # Treat the m = 0 case
-        rand1_r = np.random.normal()
-        rand1_i = 0.
-        alm_xx.real[l] = rand1_r*rms_xx
-        alm_yy.real[l] = rand1_r*rms_yy1
+		# Treat the m = 0 case
+		rand1_r = np.random.normal()
+		rand1_i = 0.
+		alm_xx.real[l] = rand1_r*rms_xx
+		alm_yy.real[l] = rand1_r*rms_yy1
 
-        #Treat the m > 0 cases
-        for m in xrange(1,l+1):
-            rand1_r = np.random.normal()/np.sqrt(2.0)
-            rand1_i = np.random.normal()/np.sqrt(2.0)
-            alm_xx.real[(m*(2*lmax+1-m)/2)+l] = rand1_r*rms_xx
-            alm_xx.imag[(m*(2*lmax+1-m)/2)+l] = rand1_i*rms_xx
-            alm_yy.real[(m*(2*lmax+1-m)/2)+l] = rand1_r*rms_yy1
-            alm_yy.imag[(m*(2*lmax+1-m)/2)+l] = rand1_i*rms_yy1
+		#Treat the m > 0 cases
+		for m in xrange(1,l+1):
+			rand1_r = np.random.normal()/np.sqrt(2.0)
+			rand1_i = np.random.normal()/np.sqrt(2.0)
+			alm_xx.real[(m*(2*lmax+1-m)/2)+l] = rand1_r*rms_xx
+			alm_xx.imag[(m*(2*lmax+1-m)/2)+l] = rand1_i*rms_xx
+			alm_yy.real[(m*(2*lmax+1-m)/2)+l] = rand1_r*rms_yy1
+			alm_yy.imag[(m*(2*lmax+1-m)/2)+l] = rand1_i*rms_yy1
 
-    # Loop on l: second term rms_yy2 of Y field coefficients
-    for l in xrange(0,lmax+1):
-        rms_yy2 = 0.
-        if clxx[l] != 0.: # To avoid division by zero
-            rms_yy2 = clyy[l] - (clxy[l]/clxx[l])*clxy[l]
-            rms_yy2 = np.sqrt(rms_yy2)
+	# Loop on l: second term rms_yy2 of Y field coefficients
+	for l in xrange(0,lmax+1):
+		rms_yy2 = 0.
+		if clxx[l] != 0.: # To avoid division by zero
+			rms_yy2 = clyy[l] - (clxy[l]/clxx[l])*clxy[l]
+			rms_yy2 = np.sqrt(rms_yy2)
 
-        # Treat the m = 0 case
-        rand2_r = np.random.normal()
-        rand2_i = 0.
-        alm_yy.real[l] = alm_yy.real[l] + rand2_r*rms_yy2
+		# Treat the m = 0 case
+		rand2_r = np.random.normal()
+		rand2_i = 0.
+		alm_yy.real[l] = alm_yy.real[l] + rand2_r*rms_yy2
 
-        #Treat the m > 0 cases
-        for m in xrange(1,l+1):
-            rand2_r = np.random.normal()/np.sqrt(2.0)
-            rand2_i = np.random.normal()/np.sqrt(2.0)
-            alm_yy.real[(m*(2*lmax+1-m)/2)+l] = alm_yy.real[(m*(2*lmax+1-m)/2)+l] + rand2_r*rms_yy2
-            alm_yy.imag[(m*(2*lmax+1-m)/2)+l] = alm_yy.imag[(m*(2*lmax+1-m)/2)+l] + rand2_i*rms_yy2
+		#Treat the m > 0 cases
+		for m in xrange(1,l+1):
+			rand2_r = np.random.normal()/np.sqrt(2.0)
+			rand2_i = np.random.normal()/np.sqrt(2.0)
+			alm_yy.real[(m*(2*lmax+1-m)/2)+l] = alm_yy.real[(m*(2*lmax+1-m)/2)+l] + rand2_r*rms_yy2
+			alm_yy.imag[(m*(2*lmax+1-m)/2)+l] = alm_yy.imag[(m*(2*lmax+1-m)/2)+l] + rand2_i*rms_yy2
 
-    return np.nan_to_num(alm_xx), np.nan_to_num(alm_yy)
+	return np.nan_to_num(alm_xx), np.nan_to_num(alm_yy)
 
 def GetCorrMaps(clxy, clxx, clyy, nside, lmax=None, pixwin=True):
-    """
-    Generates correlated gaussian realizations of two fields X and Y 
-    which statistics is described by the sets of three Auto- and 
-    Cross-spectra clxx, clyy, and clxy. 
-    
-    ! Realizations are returned as *maps* !
+	"""
+	Generates correlated gaussian realizations of two fields X and Y 
+	which statistics is described by the sets of three Auto- and 
+	Cross-spectra clxx, clyy, and clxy. 
+	
+	! Realizations are returned as *maps* !
 
-    Parameters
-    ----------
-    clxy :  array-like 
-    	Input cross-power spectrum C_l^{XY}
+	Parameters
+	----------
+	clxy :  array-like 
+		Input cross-power spectrum C_l^{XY}
 
-    clxx :  array-like 
-    	Input auto-power  spectrum C_l^{XX} of first field
+	clxx :  array-like 
+		Input auto-power  spectrum C_l^{XX} of first field
 
-    clyy :  array-like 
-    	Input auto-power  spectrum C_l^{YY} of second field
+	clyy :  array-like 
+		Input auto-power  spectrum C_l^{YY} of second field
 
-    nside : int 
-    	Healpix resolution
+	nside : int 
+		Healpix resolution
 
-    lmax : int (optional) 
-    	Maximum multipole 
+	lmax : int (optional) 
+		Maximum multipole 
 
-    pixwin : bool (optional) [def=True] 
-    	If True, Convolve with pixel window function
-    
-    Returns
-    -------
-    map_xx : array-like
-    	First field map
-    
-    map_yy : array-like
-     	Second field map
-    """
-    # if lmax is None:
-    #     lmax = clxy.size+1
+	pixwin : bool (optional) [def=True] 
+		If True, Convolve with pixel window function
+	
+	Returns
+	-------
+	map_xx : array-like
+		First field map
+	
+	map_yy : array-like
+		Second field map
+	"""
+	# if lmax is None:
+	#     lmax = clxy.size+1
 
-    alm_xx, alm_yy = GetCorrAlms(clxy, clxx, clyy, nside, lmax=lmax)
+	alm_xx, alm_yy = GetCorrAlms(clxy, clxx, clyy, nside, lmax=lmax)
 
-    # Creating XX and YY Maps
-    map_xx = hp.sphtfunc.alm2map(alm_xx, nside, pixwin=pixwin, lmax=lmax, verbose=False)
-    map_yy = hp.sphtfunc.alm2map(alm_yy, nside, pixwin=pixwin, lmax=lmax, verbose=False)
+	# Creating XX and YY Maps
+	map_xx = hp.sphtfunc.alm2map(alm_xx, nside, pixwin=pixwin, lmax=lmax, verbose=False)
+	map_yy = hp.sphtfunc.alm2map(alm_yy, nside, pixwin=pixwin, lmax=lmax, verbose=False)
 
-    return map_xx, map_yy
+	return map_xx, map_yy
+
+
+def GetNlgg(counts, mask=None, lmax=None, return_ngal=False):
+	"""
+	Returns galaxy shot-noise spectra given a number counts Healpix map. 
+	If return_ngal is True, it returns also the galaxy density in gal/ster.
+
+	Parameters
+	----------
+	counts :  array-like 
+		Healpix map containing sources counts
+
+	mask :  array-like 
+		Healpix map containing binary mask
+
+	lmax : int (optional) 
+		Maximum multipole 
+
+	return_ngal : bool (optional) [def=False] 
+		If True, returns the galaxy density in gal/ster
+	
+	Returns
+	-------
+	nlgg : array-like
+		noise power spectrum
+	
+
+	Notes
+	----
+	1. If mask is not binary, the mean 
+	2. If mask is not None, yielded spectrum is not pseudo
+	"""
+	counts = np.asarray(counts)
+
+	if lmax is None: lmax = hp.npix2nside(counts.size) * 2
+	if mask is not None: 
+		mask = np.asarray(mask)
+		fsky = np.mean(mask)
+	else: 
+		mask = 1.
+		fsky = 1.
+
+	N_tot = np.sum(counts * mask)
+	ngal  = N_tot / 4. / np.pi / fsky
+
+	if return_ngal:
+		return np.ones(lmax+1) / ngal, ngal
+	else:
+		return np.ones(lmax+1) / ngal
